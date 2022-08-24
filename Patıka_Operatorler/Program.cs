88a2ef3c4a9693e33f41a7296f07520fec6401ac
MuratKaya <97357050+muratkayaa@users.noreply.github.com>
2022-08-24 19:50:16 +0300

    Add files via upload

---
+++ Patıka_Operatorler/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Operatorler");
+ Console.WriteLine("Operatorler");
+        // Atama ve Islemli atama
+        // (=, +=, -=, *=, /=)
+        int x = 3;
+        int y =3;
+        y = y + 2;
+        Console.WriteLine(y);
+        y *= 2;
+        Console.WriteLine(y);
+        y /=2;
+         Console.WriteLine(y);
+         x+=2;
+         Console.WriteLine(x);
+
+        
+        // Mantıksal Operatörler (||, &&, !)
+        bool isSucccess =true;
+        bool isCompleted =false;
+
+        if(isSucccess && isCompleted)
+        Console.WriteLine("Perfect");
+
+         if(isSucccess || isCompleted)
+        Console.WriteLine("great");
+
+       //İlişkisel Operatörler (==,!=, <, >, >=,<=)
+       int a =3;
+       int b=4;       
+       bool v = a==b;
+        Console.WriteLine(v);
+             v= a==b;
+         Console.WriteLine(v);
+             v= a>=b;
+        Console.WriteLine(v);
+             v= a<=b;
+         Console.WriteLine(v);
+          v= a<b;
+        Console.WriteLine(v);
+          v= a>b;
+        Console.WriteLine(v);
+
+
+        //Aritmetik (+, -, *, /, %, ++, --)
+
+
+        
